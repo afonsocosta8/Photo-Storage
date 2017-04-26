b69dbd27d11b47f3bfe24165ac6d5b38712ee445
afonsocosta8 <afonside@hotmail.com>
2017-04-26 13:38:34 +0100

    starting galleryconnect

--- api.c
+++ api.c
@@ -1,26 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+
 int gallery_connect(char * host, in_port_t p){
 
-  struct sockaddr_un server_addr;
-	char buff[100];
+  struct sockaddr_in server_addr;
+	struct sockaddr_in client_addr;
+	char buff[]="GET";
 	int nbytes;
 
-	int sock_fd= socket(AF_UNIX, SOCK_DGRAM, 0);
+	int sock_fd= socket(AF_INET, SOCK_DGRAM, 0);
 
 	if (sock_fd == -1){
 		perror("socket: ");
 		exit(-1);
 	}
 
-  printf(" socket created \n Ready to send\n");
+	printf(" socket created \n Ready to send\n");
 
-	server_addr.sun_family = AF_UNIX;
-	strcpy(server_addr.sun_path, SOCK_ADDRESS);
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(p);
+	inet_aton(host, &server_addr.sin_addr);
 
 	sprintf(buff, "GET");
 	nbytes = sendto(sock_fd,
 	                    buff, strlen(buff)+1, 0,
 	                    (const struct sockaddr *) &server_addr, sizeof(server_addr));
 	printf("sent %d %s\n", nbytes, buff);
+	nbytes = recv(sock_fd, buff, 20, 0);
+	printf("received %d bytes --- %s ---\n", nbytes, buff);
 }
 
 uint32_t gallery_add_photo(int peer_socket, char *file_n){
