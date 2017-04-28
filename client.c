381e5251dd45d49268ce7bda8582b8c158c8a5ca
afonsocosta8 <afonside@hotmail.com>
2017-04-28 12:03:30 +0100

    gateway connect

---
+++ client.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include "PhotoStorageApi.h"
+
+int main(int argc, char const *argv[]) {
+  char *host, *port;
+  port = calloc(128, sizeof(char));
+  host = calloc(128, sizeof(char));
+  int p;
+  int i;
+  if(argc!=5) { /* Number of arguments inserted is not correct */
+      printf("Something went wrong...\nUsage: schat -n <name>.<survame> -i <ip> -p <scport> -s <snpip> -q <snpport>\n");
+      exit(4);
+  }else{ /*Reading each one of the arguments */
+      for(i=1; i<argc; i=i+2) {
+              switch(argv[i][1]) {
+
+              case 'h':
+                      strcpy(host, argv[i+1]);
+                      break;
+
+              case 'p':
+                      strcpy(port, argv[i+1]);
+                      break;
+
+              default:
+                      printf("Something went wrong...\nUsage: schat -n <name>.<survame> -i <ip> -p <scport> -s <snpip> -q <snpport>\n");
+                      exit(-1);
+                      break;
+              }
+      }
+  }
+  p=atoi(port);
+  gallery_connect(host, p);
+  return 0;
+}
