01a22694d8fe9350753c081fac97f2a001a8280c
Bruno Gonçalves <btoztas@gmail.com>
2017-05-29 18:15:20 +0100

    done

--- client2.c
+++ client2.c
@@ -48,15 +48,20 @@ int main(int argc, char const *argv[]) {
   char photo_name[100];
   char keyword[20];
   char query[15];
+  int ret;
 
   while (1) {
-    printf("Options:\n");
-    printf("addphoto - add a photo to the database\n");
-    printf("getphoto - get a photo from the data base[you must provide the id and name]\n");
-    printf("getname - get the name of a photo[you must provide the id]\n");
-    printf("addkeyword - add a keyword to a photo[you must provide the keyword a photo id]\n");
-    printf("search - search for a photo by keyword[you must provide the keyword]\n");
-    printf("delete - delete a photo from the database[you must provide the id]\n");
+    printf("**************************************************************************************************\n*\n");
+    printf("*        Options:\n*\n");
+    printf("*            addphoto - add a photo to the database\n*\n");
+    printf("*            getphoto - get a photo from the data base[you must provide the id and name]\n*\n");
+    printf("*            getname - get the name of a photo[you must provide the id]\n*\n");
+    printf("*            addkeyword - add a keyword to a photo[you must provide the keyword a photo id]\n*\n");
+    printf("*            search - search for a photo by keyword[you must provide the keyword]\n*\n");
+    printf("*            delete - delete a photo from the database[you must provide the id]\n*\n");
+    printf("*            quit - exit the program\n*\n");
+    printf("**************************************************************************************************\n\n");
+    printf("Enter command: ");
     fscanf(stdin, "%s", query);
     if(strcmp(query, "addphoto")==0){
       printf("enter the photo name:\n");
@@ -64,43 +69,105 @@ int main(int argc, char const *argv[]) {
       psock = gallery_connect(host, p);
       if(psock>0){
         photo_id1 = gallery_add_photo(psock, photo_name);
+        if(photo_id1>0){
+          printf("\nPhoto added successfully. Your photo ID is %d\n\n", photo_id1);
+        }else{
+          printf("\nInvalid file_name or problems in communication with the server\n\n");
+        }
+      }else if(psock==0){
+        printf("\nNo peer is available\n\n");
+      }else{
+        printf("\nGateway cannot be accessed\n\n");
       }
     }else if(strcmp(query, "getphoto")==0){
       printf("enter the id and name[by this order and spaced]:\n");
+      fscanf(stdin, "%d %s", &photo_id1, photo_name);
+      psock = gallery_connect(host, p);
+      if(psock>0){
+        ret = gallery_get_photo(psock, photo_id1, photo_name);
+        if(ret==1){
+          printf("\nPhoto received\n\n");
+        }else if(ret==0){
+          printf("\nPhoto does not exist\n\n");
+        }else{
+          printf("\ninvalid arguments or network problem\n\n");
+        }
+      }else if(psock==0){
+        printf("\nNo peer is available\n\n");
+      }else{
+        printf("\nGateway cannot be accessed\n\n");
+      }
     }else if(strcmp(query, "getname")==0){
       printf("enter the photo id:\n");
       fscanf(stdin, "%d", &photo_id1);
       psock = gallery_connect(host, p);
       if(psock>0){
-        gallery_get_photo_name(psock, photo_id1, name);
+        ret = gallery_get_photo_name(psock, photo_id1, name);
+        if(ret==1){
+          printf("\nPhoto with ID %d is named %s\n\n", photo_id1, (*name));
+        }else if(ret==0){
+          printf("\nPhoto does not exist\n\n");
+        }else{
+          printf("\ninvalid arguments or network problem\n\n");
+        }
+      }else if(psock==0){
+        printf("\nNo peer is available\n\n");
+      }else{
+        printf("\nGateway cannot be accessed\n\n");
       }
-      printf("printing name:\n");
-      printf("name is %s\n", (*name));
     }else if(strcmp(query, "addkeyword")==0){
       printf("enter the photo id and keyword to add[by this order and spaced]:\n");
       fscanf(stdin, "%d %s", &photo_id1, keyword);
       psock = gallery_connect(host, p);
       if(psock>0){
-        gallery_add_keyword(psock, photo_id1, keyword);
+        ret = gallery_add_keyword(psock, photo_id1, keyword);
+        if(ret==1){
+          printf("\nKeyword added successfully\n\n");
+        }else if(ret==0){
+          printf("\nPhoto does not exist\n\n");
+        }else{
+          printf("\ninvalid arguments or network problem\n\n");
+        }
+      }else if(psock==0){
+        printf("\nNo peer is available\n\n");
+      }else{
+        printf("\nGateway cannot be accessed\n\n");
       }
     }else if(strcmp(query, "search")==0){
       printf("enter the keyword:\n");
       fscanf(stdin, "%s", keyword);
       psock = gallery_connect(host, p);
       if(psock>0){
-        gallery_search_photo(psock, keyword, ids);
+        ret = gallery_search_photo(psock, keyword, ids);
+      }else if(psock==0){
+        printf("\nNo peer is available\n\n");
+      }else{
+        printf("\nGateway cannot be accessed\n\n");
       }
     }else if(strcmp(query, "delete")==0){
       printf("enter the photo id:\n");
       fscanf(stdin, "%d", &photo_id1);
       psock = gallery_connect(host, p);
       if(psock>0){
-        gallery_delete_photo(psock, photo_id1);
+        ret = gallery_delete_photo(psock, photo_id1);
+        if(ret==1){
+          printf("Photo deleted\n");
+        }else if(ret==0){
+          printf("\nPhoto does not exist\n\n");
+        }else{
+          printf("\ninvalid arguments or network problem\n\n");
+        }
+      }else if(psock==0){
+        printf("\nNo peer is available\n\n");
+      }else{
+        printf("\nGateway cannot be accessed\n\n");
       }
     }else if(strcmp(query, "quit")==0){
       printf("Goodbye\n");
       close(psock);
       return 0;
+    }else{
+      printf("\n\"%s\" command is not valid\n\n", query);
     }
   }
 
@@ -109,3 +176,4 @@ int main(int argc, char const *argv[]) {
 
   return 0;
 }
+
